68cbfa6234d939eebb97ce45b50fb2d42b92efa8
kikiliu7 <161527266+kikiliu7@users.noreply.github.com>
2026-04-16 15:37:13 -0400

    console logs debug

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ import AssignmentRoutes from "./kambaz/assignments/routes.js";
 
 
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+console.log("CONNECTION_STRING:", process.env.DATABASE_CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
